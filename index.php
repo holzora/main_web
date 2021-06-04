45599e2a13eaab97f8d7bd213d3392b52ef561a0
holzora <holzora@foxmail.com>
2021-06-04 14:17:50 +0800

    update

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-<?php require("./page/header.php"); ?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <link rel="shortcut icon" href="img/favicon.png"/>
+    <link rel="stylesheet" href="css/main.css">
+    <title>霍西佐拉</title>
+</head>
+<body>
     <div id=header>
         <div id=title>
             <ul class="left">
@@ -11,4 +24,11 @@
             </ul>
         </div>
     </div>
-<?php require("./page/footer.php"); ?>
+    <div id=footer>
+        <div id=time>
+            <span id="runtime_span"></span>
+        </div>
+    </div>
+<script src="js/main.js"></script>
+</body>
+</html>
