37dbd611e4c76ba196ea328389995ca767e3b67a
holzora <holzora@foxmail.com>
2021-06-04 15:33:25 +0800

    update

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function disptime(){
     var mytime = setInterval("disptime()",1000);
 
 function show_runtime(){window.setTimeout("show_runtime()",1000);
-    X=new Date("05/14/2021 23:00:00");
+    X=new Date("06/04/2021 14:00:00");
     Y=new Date();T=(Y.getTime()-X.getTime());M=24*60*60*1000;
     a=T/M;A=Math.floor(a);b=(a-A)*24;B=Math.floor(b);c=(b-B)*60;C=Math.floor((b-B)*60);D=Math.floor((c-C)*60);
     runtime_span.innerHTML="本站已运行: "+A+"天"+B+"小时"+C+"分"+D+"秒"}show_runtime();
