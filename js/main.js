eb1dc69213c386ab9e15584f40343e7c24f65987
holzora <holzora@foxmail.com>
2021-06-06 15:42:42 +0800

    update more...

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function disptime(){
     var mm=today.getMinutes();  
     var ss = today.getSeconds();  
     document.getElementById("myclock").innerHTML="<span>"+hh+":"+mm+":"+ss+"</span>"  
-     }
+    }
     var mytime = setInterval("disptime()",1000);
 
 function show_runtime(){window.setTimeout("show_runtime()",1000);
@@ -12,3 +12,20 @@ function show_runtime(){window.setTimeout("show_runtime()",1000);
     Y=new Date();T=(Y.getTime()-X.getTime());M=24*60*60*1000;
     a=T/M;A=Math.floor(a);b=(a-A)*24;B=Math.floor(b);c=(b-B)*60;C=Math.floor((b-B)*60);D=Math.floor((c-C)*60);
     runtime_span.innerHTML="本站已运行: "+A+"天"+B+"小时"}show_runtime();
+
+$(function () {
+	var windoww = $(window).width();
+	$('.sp_nav').click(function () {
+		$('.sp_nav').toggleClass('sp_nav_se');
+		$('.sjj_nav').toggleClass('nav_show');
+	});
+	
+	$('.sjj_nav ul li i').click(function () {
+		$(this).parent().children('ul').slideToggle().parent().siblings().children('ul').slideUp();
+		$(this).toggleClass('sjj_nav_i_se');
+		$(this).parent().siblings().find('i').removeClass('sjj_nav_i_se');
+	});
+	
+	$('.sp_nav_xjb').html('<svg t="1496193951932" class="icon" style="" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="995" xmlns:xlink="http://www.w3.org/1999/xlink" width="200" height="200"> <path d="M768.468 428.876l-84.723-84.723-170.711 170.711-170.711-170.711-84.723 84.723 170.711 170.711-0.092 0.091 84.723 84.724 0.092-0.092 0.092 0.092 84.723-84.724-0.092-0.091z" p-id="996"></path></svg>');
+	
+});
